520cd94af7dc8a2514066e79d3336ec1e61c916c
linusvdv <30822552+linusvdv@users.noreply.github.com>
2017-04-30 15:19:51 +0200

    Rokade gemacht

--- rokade.cpp
+++ rokade.cpp
@@ -18,29 +18,29 @@ bool testen_ob_man_es_darf(int felt[8][8], int zurueckgeben){
         if(felt[0][0]!=3)
             ooo_o = false;
     }
-    else if(koenig_o==true){
+    if(koenig_o==true){
         if(felt[0][4]!=5){
             koenig_o = false;
             ooo_o = false;
             oo_o = false;
         }
     }
-    else if(oo_o==true){
+    if(oo_o==true){
         if(felt[0][7]!=3)
             oo_o = false;
     }
-    else if(ooo_x==true){
+    if(ooo_x==true){
         if(felt[7][0]!=-3)
             ooo_x = false;
     }
-    else if(koenig_o==true){
+    if(koenig_o==true){
         if(felt[7][4]!=-5){
              koenig_x = false;
              ooo_x = false;
              oo_x = false;
         }
     }
-    else if(oo_x==true){
+    if(oo_x==true){
         if(felt[7][7]!=-3)
             oo_x = false;
     }
@@ -63,25 +63,37 @@ bool rokade(int felt[8][8], int zahl[4], bool ifrokade){
        if(((zahl[0]==0) && (zahl[1]==4))
        && ((zahl[2]==0) && (zahl[3]==2)))
            if((felt[0][1]==0) && (felt[0][2]==0) && (felt[0][3]==0))
-               if(testen_ob_man_es_darf(felt, 1)==true)
+               if(testen_ob_man_es_darf(felt, 1)==true){
+                   felt[0][3]= 3;
+                   felt[0][0]= 0;
                    return true;
+               }
 
        if(((zahl[0]==0) && (zahl[0]==4))
        && ((zahl[0]==0) && (zahl[0]==6)))
            if((felt[0][5]==0) && (felt[0][6]==0))
-               if(testen_ob_man_es_darf(felt, 3)==true)
+               if(testen_ob_man_es_darf(felt, 3)==true){
+                   felt[0][7]= 3;
+                   felt[0][5]= 0;
                    return true;
+               }
 
        if(((zahl[0]==7) && (zahl[1]==4))
        && ((zahl[2]==7) && (zahl[3]==2)))
            if((felt[7][1]==0) && (felt[7][2]==0) && (felt[7][3]==0))
-               if(testen_ob_man_es_darf(felt, 2)==true)
+               if(testen_ob_man_es_darf(felt, 2)==true){
+                   felt[0][3]=-3;
+                   felt[0][0]= 0;
                    return true;
+               }
 
        if(((zahl[0]==7) && (zahl[0]==4))
        && ((zahl[0]==7) && (zahl[0]==6)))
            if((felt[7][5]==0) && (felt[7][6]==0))
-               if(testen_ob_man_es_darf(felt, 4)==true)
+               if(testen_ob_man_es_darf(felt, 4)==true){
+                   felt[0][7]= 3;
+                   felt[0][5]= 0;
                    return true;
+               }
     }
 }
