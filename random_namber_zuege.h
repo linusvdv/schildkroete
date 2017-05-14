a80e35a57b0137dffbc4720f13c08012b7925fe6
linusvdv <30822552+linusvdv@users.noreply.github.com>
2017-05-14 15:39:47 +0200

    vergessene files

---
+++ random_namber_zuege.h
@@ -0,0 +1,1 @@
+bool random_namber_zuege(int felt[8][8], array<int,4>& Zahl, int farbe, int enpassenttester, int ifgenerator);
---
+++ stet_der_koenig_schach.h
@@ -0,0 +1,1 @@
+bool stet_der_koenig_schach(int felt[8][8], int farbe, int enpassenttester);
