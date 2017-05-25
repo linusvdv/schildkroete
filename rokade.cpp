34f3ee9292eb8fd922157eafa91f02f67a9c613b
linusvdv <30822552+linusvdv@users.noreply.github.com>
2017-05-25 19:05:31 +0200

    rokade gemacht nur noch promovierengit diff!

--- rokade.cpp
+++ rokade.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 #include "rokade.h"
-#include "FEN_leser.h"
+#include "stet_der_koenig_schach.h"
 #include "types.h"
 
 using namespace std;
@@ -11,9 +11,9 @@ void testen_ob_man_es_darf(position& pos){
     //o = w = weis
     //x = s = schwarz 
     if(pos.felt[0][0]!=3)
-        pos.rokaden[2] = false;
+        pos.rokaden[1] = false;
     if(pos.felt[0][4]!=5){
-        pos.rokaden[2] = false;
+        pos.rokaden[1] = false;
         pos.rokaden[0] = false;
     }
     if(pos.felt[0][7]!=3)
@@ -23,35 +23,39 @@ void testen_ob_man_es_darf(position& pos){
         pos.rokaden[3] = false;
     if(pos.felt[7][4]!=-5){
         pos.rokaden[3] = false;
-        pos.rokaden[1] = false;
+        pos.rokaden[2] = false;
     }
     if(pos.felt[7][7]!=-3)
-        pos.rokaden[1] = false;
+        pos.rokaden[2] = false;
 }
 
 bool rokade(position& pos, zuege& zug){
     testen_ob_man_es_darf(pos);
-    if(((zug.Zahl[0]==0) && (zug.Zahl[1]==4))
-    && ((zug.Zahl[2]==0) && (zug.Zahl[3]==2)))
-        if((pos.felt[0][1]==0) && (pos.felt[0][2]==0) && (pos.felt[0][3]==0))
-            if(pos.rokaden[1]==true){
-                pos.felt[0][3]= 3;
-                pos.felt[0][0]= 0;
-                return true;
-            }
+    position pos2 = pos;
+    pos2.farbe*=-1;
+    if(pos.farbe==1){
+        if(((zug.Zahl[0]==0) && (zug.Zahl[1]==4))
+        && ((zug.Zahl[2]==0) && (zug.Zahl[3]==2)))
+            if((pos.felt[0][1]==0) && (pos.felt[0][2]==0) && (pos.felt[0][3]==0) && (stet_der_koenig_schach(pos2, 0, 3)==false) && (stet_der_koenig_schach(pos2, 0, 4)==false))
+                if(pos.rokaden[1]==true){
+                    pos.felt[0][3]= 3;
+                    pos.felt[0][0]= 0;
+                    return true;
+                }
 
-    if(((zug.Zahl[0]==0) && (zug.Zahl[1]==4))
-    && ((zug.Zahl[2]==0) && (zug.Zahl[3]==6)))
-        if((pos.felt[0][5]==0) && (pos.felt[0][6]==0))
-            if(pos.rokaden[0]==true){
-                pos.felt[0][7]= 0;
-                pos.felt[0][5]= 3;
-                return true;
-            }
-
+        if(((zug.Zahl[0]==0) && (zug.Zahl[1]==4))
+        && ((zug.Zahl[2]==0) && (zug.Zahl[3]==6)))
+            if((pos.felt[0][5]==0) && (pos.felt[0][6]==0) && (stet_der_koenig_schach(pos2, 0, 4)==false) && (stet_der_koenig_schach(pos2, 0, 5)==false))
+                if(pos.rokaden[0]==true){
+                    pos.felt[0][7]= 0;
+                    pos.felt[0][5]= 3;
+                    return true;
+                }
+        return false;
+    }
     if(((zug.Zahl[0]==7) && (zug.Zahl[1]==4))
     && ((zug.Zahl[2]==7) && (zug.Zahl[3]==2)))
-        if((pos.felt[7][1]==0) && (pos.felt[7][2]==0) && (pos.felt[7][3]==0))
+        if((pos.felt[7][1]==0) && (pos.felt[7][2]==0) && (pos.felt[7][3]==0) && (stet_der_koenig_schach(pos2, 7, 3)==false) && (stet_der_koenig_schach(pos2, 7, 4)==false))
             if(pos.rokaden[3]==true){
                 pos.felt[7][3]=-3;
                 pos.felt[7][0]= 0;
@@ -60,7 +64,7 @@ bool rokade(position& pos, zuege& zug){
 
     if(((zug.Zahl[0]==7) && (zug.Zahl[1]==4))
     && ((zug.Zahl[2]==7) && (zug.Zahl[3]==6)))
-        if((pos.felt[7][5]==0) && (pos.felt[7][6]==0))
+        if((pos.felt[7][5]==0) && (pos.felt[7][6]==0) && (stet_der_koenig_schach(pos2, 7, 4)==false) && (stet_der_koenig_schach(pos2, 7, 5)==false))
             if(pos.rokaden[2]==true){
                 pos.felt[7][7]= 0;
                 pos.felt[7][5]=-3;
--- stet_der_koenig_schach.cpp
+++ stet_der_koenig_schach.cpp
@@ -6,18 +6,24 @@
 
 using namespace std;
 
-bool stet_der_koenig_schach(position& pos){
+bool stet_der_koenig_schach(position pos, int zeile, int spalte){
 
     position pos2;
     zuege zug;
-
-    for(int i=0; i<8; i++)
-        for(int j=0; j<8; j++){
-            if(pos.felt[i][j]==-5*pos.farbe){
-                zug.Zahl[2]=i;
-                zug.Zahl[3]=j;
+    if(zeile==8)
+        for(int i=0; i<8; i++)
+            for(int j=0; j<8; j++){
+                if(pos.felt[i][j]==-5*pos.farbe){
+                    zug.Zahl[2]=i;
+                    zug.Zahl[3]=j;
+                }
             }
-        }
+    else{
+        zug.Zahl[2]=zeile;
+        zug.Zahl[3]=spalte;
+        pos.felt[zeile][spalte]=-5*pos.farbe;
+        pos.felt[((pos.farbe+1)/2*-1+1)*7][4]=0;
+    }
 
     bool schach=false;
     
