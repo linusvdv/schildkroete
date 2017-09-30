b9c0c05461c6f9dd3407c5f2d700895f17fb89ec
linusvdv <30822552+linusvdv@users.noreply.github.com>
2017-09-30 15:29:40 +0200

    endbewertung

--- bewertung.cpp
+++ bewertung.cpp
@@ -74,64 +74,89 @@ feldType Koenig=  {{       // weiss
                   {{-30,-40,-40,-50,-50,-40,-40,-30}},
                   }} ;     // schwarz
 
+feldType Koenigentspiel={{       // weiss
+                        {{-50,-30,-30,-30,-30,-30,-30,-50}},
+                        {{-30,-30,  0,  0,  0,  0,-30,-30}},
+                        {{-30,-10, 20, 30, 30, 20,-10,-30}},
+                        {{-30,-10, 30, 40, 40, 30,-10,-30}},
+                        {{-30,-10, 30, 40, 40, 30,-10,-30}},
+                        {{-30,-10, 20, 30, 30, 20,-10,-30}},
+                        {{-30,-20,-10,  0,  0,-10,-20,-30}},
+                        {{-50,-40,-30,-20,-20,-30,-40,-50}},
+                        }} ;     // schwarz
+
 int bewertung(const position& pos){ 
     nodes++;
 
-    int gute=0, doppel_p[2]={}, doppel_r[2]={}, leufer[2]={};
+    int gute=0, doppel_p[2]={}, doppel_r[2]={}, leufer[2]={}, entspiel=0, wo_koenig[2][2]={};
     for(int j=0; j<8; j++){
         for(int i=0; i<8; i++)
             switch(pos.felt[i][j]){
                 case  1:
+                    entspiel+=320;
                     gute+=320;
                     gute+=Pferd[i][j];
                     break;
                 case -1:
+                    entspiel+=320;
                     gute-=320;
                     gute-=Pferd[7-i][j];
                     break;
                 case  2:
+                    entspiel+=330;
                     gute+=330;
                     gute+=Leufer[i][j];
                     leufer[0]++;
                     break;
                 case -2:
+                    entspiel+=330;
                     gute-=330;
                     gute-=Leufer[7-i][j];
                     leufer[1]++;
                     break;
                 case  3:
+                    entspiel+=500;
                     gute+=500;
                     gute+=Turm[i][j];
                     doppel_r[0]+=1;
                     break;
                 case -3:
+                    entspiel+=500;
                     gute-=500;
                     gute-=Turm[7-i][j];
                     doppel_r[1]+=1;
                     break;
                 case  4:
+                    entspiel+=900;
                     gute+=Dame[i][j];
                     gute+=900;
                     break;
                 case -4:
+                    entspiel+=900;
                     gute-=900;
                     gute-=Dame[7-i][j];
                     break;
                 case  5:
-                    gute+=Koenig[i][j];
+                    wo_koenig[1][0]=i;
+                    wo_koenig[1][1]=j;
                     break;
                 case -5:
-                    gute-=Koenig[7-i][j];
+                    wo_koenig[0][0]=7-i;
+                    wo_koenig[0][1]=j;
                     break;
                 case  6:
+                    entspiel+=100;
                     gute+=100;
                     gute+=Bauer[i][j];
                     doppel_p[0]+=1;
                     break;
                 case -6:
+                    entspiel+=100;
                     gute-=100;
                     gute-=Bauer[7-i][j];
                     doppel_p[1]+=1;
+                    break;
+                default:
                     break;
             }
         gute-= doppel_p[0]>=2 ? (doppel_p[0]-1)*30 : 0;
@@ -146,7 +171,14 @@ int bewertung(const position& pos){
 
     if (leufer[0]>1) gute+=30;
     if (leufer[1]>1) gute-=30;
-
+    if (entspiel<2000){
+       gute+=Koenigentspiel[wo_koenig[1][0]][wo_koenig[1][1]];
+       gute-=Koenigentspiel[wo_koenig[0][0]][wo_koenig[0][1]];
+    }
+    else{
+        gute+=Koenig[wo_koenig[1][0]][wo_koenig[1][1]];
+        gute-=Koenig[wo_koenig[0][0]][wo_koenig[0][1]];
+    }
     gute*=pos.farbe;
     return gute;
 }
